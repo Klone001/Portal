0b1996962a6fadebf6cb092f2d21fbef7fa4d34b
Solomon13693 <adeoyesolomon2693@gmail.com>
2024-10-19 02:36:50 +0100

    [MVP-1]: First commit

--- lib/fonts.tsx
+++ lib/fonts.tsx
@@ -3,27 +3,27 @@ import localFont from 'next/font/local'
 const EFCircular = localFont({
   src: [
     {
-      path: '/fonts/EFCIRCULAR-LIGHT.otf',
+      path: '../public/fonts/EFCIRCULAR-LIGHT.otf',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '/fonts/EFCIRCULAR-BOOK.otf',
+      path: '../public/fonts/EFCIRCULAR-BOOK.otf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '/fonts/EFCIRCULAR-MEDIUM.otf',
+      path: '../public/fonts/EFCIRCULAR-MEDIUM.otf',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '/fonts/EFCIRCULAR-BOLD.otf',
+      path: '../public/fonts/EFCIRCULAR-BOLD.otf',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '/fonts/EFCIRCULAR-BLACK.otf',
+      path: '../public/fonts/EFCIRCULAR-BLACK.otf',
       weight: '900',
       style: 'normal',
     },
